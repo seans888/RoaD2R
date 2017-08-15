6bc4b2ea63ee68403e898e4d9b3de55123704ede
acdeleon <deleonaleoralphcastro@gmail.com>
2017-08-15 19:52:03 +0800

    update

--- Prototype/Project Wyatt/content.js
+++ Prototype/Project Wyatt/content.js
@@ -1,2 +1,6 @@
-// content.js
-alert("Hello from your Chrome extension!")
+alert("update")
+$(document).on("click", "a", function() {
+    //this == the link that was clicked
+    var href = $(this).attr("href");
+    //alert("You're trying to go to " + href);
+});
