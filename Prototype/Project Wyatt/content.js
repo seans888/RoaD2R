4c1e70a4f9af7e24ed463280b19e9a51e961fe3a
acdeleon <deleonaleoralphcastro@gmail.com>
2017-08-16 15:50:25 +0800

    update content

--- Prototype/Project Wyatt/content.js
+++ Prototype/Project Wyatt/content.js
@@ -1,11 +1,11 @@
 $(document).on("click", "a", function() {
    	//this == the link that was clicked
    	   var href = $(this).attr("href");
-    	if (href.indexOf("gmanetwork") !== -1){
+    	/*if (href.indexOf("gmanetwork") !== -1){
     		alert("Accessing Gma");
-    	}
-    if(href.search("news.abs-cbn") !== -1){
-    	alert("Accessing abs-cbn");
+    	}*/
+    if(href.search("news.abs-cbn"||"gmanetwork") !== -1){
+    	alert("This website is authentic");
     }
 });
 
