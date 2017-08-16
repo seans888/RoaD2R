91fbbaa075a932aef1a01de4b67b4380a2ba46fd
mnares <marcnares1118@gmail.com>
2017-08-16 21:22:34 +0800

    working code

--- Prototype/Project Wyatt/content.js
+++ Prototype/Project Wyatt/content.js
@@ -1,12 +1,17 @@
 
 $(document).on("click", "a", function() {
    	   var href = $(this).attr("href");
-    if(href.search("gmanetwork|news.abs-cbn.com") !== -1){
+    if(href.search("gmanetwork|gmanews|news.abs-cbn.com") !== -1){
     	alert("This website is authentic");
+	}
+
+	if(href.search("adobochronicles") !== -1) {
+		alert("This website is fake");
+	}
+/*	else{
+		alert("This website is unknown")
 }
-else{
-	alert("This website is fake")
-}
+*/
 })
 
   
