6b7d97e29458584e647547dda10e4f9449d4ea4d
jrcruz2 <josh.spark.cruz@gmail.com>
2017-08-16 20:46:12 +0800

    update

--- Prototype/Project Wyatt/content.js
+++ Prototype/Project Wyatt/content.js
@@ -9,29 +9,16 @@ else{
 }
 })
 
-
-        if (href.indexOf("gmanetwork") !== -1){
-            alert("Accessing GMA News");
-        }
-        if(href.search("news.abs-cbn") !== -1){
-        alert("Accessing ABS-CBN News");
-
-    }
-
-    if (http.search("adobochronicles") !== -1) {
-        alert("This site is found to be Fake!");
-    }
-
-
-    
-
-
+  
 window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
  
 window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
 window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
  
 if (!window.indexedDB) {
-   window.alert("Your browser doesn't support a stable version of IndexedDB.")
+   window.alert("Your browser doesn't support a stable version of IndexedDB.");
+}
+else{
+	window.alert("Database is supported in this browser");
 }
 
