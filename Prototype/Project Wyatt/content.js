292369f520c57119dacc27e1a09b47ad98d4eaf8
acdeleon <deleonaleoralphcastro@gmail.com>
2017-08-29 06:09:50 +0800

    update

--- Prototype/Project Wyatt/content.js
+++ Prototype/Project Wyatt/content.js
@@ -13,13 +13,8 @@ else{
 	}
 */
 })
-
-  
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
  
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
- 
+ //Database
 if (!window.indexedDB) {
    window.alert("Your browser doesn't support a stable version of IndexedDB.");
 }
